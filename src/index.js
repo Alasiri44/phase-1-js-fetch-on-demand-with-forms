098ec292ad6cc1a5a76d3284c4916e9e5c750078
alasiri <austinalasiri44@gmail.com>
2025-03-19 17:46:14 +0300

    Finfished the fetch function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Movie not found!"); // Handle 404 or other errors
+              }
+              return response.json();
+        })
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+  
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        }).catch((error) => {
+            const message = document.querySelector("#errorMessage");
+            message.innerText = error.message;
+            message.style.color = "red";
+          });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
